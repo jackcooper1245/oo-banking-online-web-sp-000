0cbb824a9a93c0ecf7ed27d34f5b8b85295cc242
Jack Cooper <jackcooper1245@gmail.com>
2020-04-20 16:16:17 +0000

    Automatically backed up by Learn

--- lib/transfer.rb
+++ lib/transfer.rb
@@ -1,11 +1,3 @@
 class Transfer
 
-def initialize(bank_account, bank_account, number)
-#Acts as a space for the transaction between 2 BankAccount instances
-#Check the validity before transaction occurs
-#Reject a transfer if that accounts aren’t valid or the account doesn’t have the money
-#Transfers start out “pending”. However, can be switched to “complete”, “rejected”.
-#Should be able to reverse the transfer and change the state to “reversed”.
-end
-
 end
