e2afc589eb40d2291b9d69b52df6a593b82cc204
Jack Cooper <jackcooper1245@gmail.com>
2020-04-20 17:27:40 +0000

    Automatically backed up by Learn

--- lib/transfer.rb
+++ lib/transfer.rb
@@ -1,20 +1,18 @@
 class Transfer
+  attr_accessor :sender, :receiver, :amount, :status
 
-attr_accessor :status, :sender, :receiver, :amount, :bank_account
+  def initialize(sender, receiver, amount)
+    @sender = sender
+    @receiver = receiver
+    @amount = amount
+    @status = "pending"
+  end
 
+  def valid?
+    @sender.valid? && @receiver.valid? ? true : false
+  end
 
-def initialize(sender, receiver, amount)
-@status = "pending"
-@amount = amount
-@sender = sender
-@receiver = receiver
-end
-
-def valid?
-  @sender.valid? && @receiver.valid? ? true : false
-end
-
-def execute_transaction
+  def execute_transaction
     if @sender.balance > @amount && @status == "pending"
       @sender.balance -= @amount
       @receiver.balance += @amount
@@ -25,13 +23,12 @@ def execute_transaction
     end
   end
 
-
-def reverse_transfer
-  if @status == "complete"
-  @receiver.balance -= @amount
-  @sender.balance += @amount
-  @status = "reversed"
-end
-end
-
+  def reverse_transfer
+    if @status == "complete"
+      @sender.balance += @amount
+      @receiver.balance -= @amount
+      @status = "reversed"
+    end
+  end
+  
 end
