e31b615d6be7fb216c896ed86c461fa450b78f5d
Jack Cooper <jackcooper1245@gmail.com>
2020-04-20 16:59:22 +0000

    Automatically backed up by Learn

--- lib/transfer.rb
+++ lib/transfer.rb
@@ -19,7 +19,7 @@ def execute_transaction
   @receiver.balance += @amount
   @status = "complete"
   define_singleton_method(:execute_transaction){}
-if !valid?
+else 
   @status = "rejected"
   return "Transaction rejected. Please check your account balance."
 end
