f36c9edd06b2e9d9cf2e6afca4f7d921a42a97f6
Jack Cooper <jackcooper1245@gmail.com>
2020-04-20 16:54:22 +0000

    Automatically backed up by Learn

--- lib/transfer.rb
+++ lib/transfer.rb
@@ -18,7 +18,7 @@ def execute_transaction
   @sender.balance -= @amount
   @receiver.balance += @amount
   @status = "complete"
-  define_singleton_method(:execute_transaction) 
+  define_singleton_method(:execute_transaction)
 end
 
 
