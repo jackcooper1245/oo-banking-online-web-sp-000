e8537c8498c00861e10cfd6541400236615d85bb
Jack Cooper <jackcooper1245@gmail.com>
2020-04-20 16:20:45 +0000

    Automatically backed up by Learn

--- lib/transfer.rb
+++ lib/transfer.rb
@@ -1,3 +1,20 @@
 class Transfer
 
+attr_accessor :status, 
+#initialize
+def initialize(bank_account, bank_account, amount = nil)
+# can initialize a transfer
+# initializes with a sender
+@sender = bank_account
+# initialized with a reveiver
+@receiver = bank_account
+# initializes with the status "pending"
+@status = "pending"
+# initializes with a transfer amount
+amount = nil
 end
+
+
+
+end
+
